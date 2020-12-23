e86f3f3ec4d58edb86919a1b184f1c3f64c0ceb8
MarcRichet <mrichet.cb@gmail.com>
2020-12-23 17:01:35 +0100

    Fix: Problem with user's Permissions

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { View } from 'react-native';
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
-
-import MessagesScreen from './app/screens/MessagesScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
-
-
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return (
-    
-      <MessagesScreen/>
-    
-  );
+  const requestPermission = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA);
+    result.granted
+    //const { granted } = await ImagePicker.requestCameraPermissionsAsync; // Méthode 'request'
+    if (!granted) alert("You need to enable permission to access the library");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
