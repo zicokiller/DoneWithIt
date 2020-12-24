92ae4018234d48c8d8e5fe07e12b738f4bb178b8
MarcRichet <mrichet.cb@gmail.com>
2020-12-24 16:07:27 +0100

    Feature: Add Touchable, load and delete options in ImageInput and make it reusable

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-//import * as Permissions from "expo-permissions";
 import { Button, Image } from "react-native";
 
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  //const [permission, askForPermission] = usePermissions(Permissions.MEDIA_LIBRARY, {ask : true });
-
   const [imageUri, setImageUri] = useState();
   const requestPermission = async () => {
-    //const result = await Permissions.askAsync(Permissions.MEDIA_LIBRARY)
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("You need to enable permission to access the library");
   };
@@ -29,10 +25,10 @@ export default function App() {
   };
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      <ImageInput imageUri={imageUri} />
       
+      <ImageInput imageUri={imageUri}
+      onChangeImage={uri => setImageUri(uri)} 
+      />
     </Screen>
   );
 }
